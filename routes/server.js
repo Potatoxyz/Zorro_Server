2e534196fe4276289f96cbacc40c6696efec6de0
PotatoGG <512006634@qq.com>
2018-02-27 18:33:34 +0800

    jwt

--- routes/server.js
+++ routes/server.js
@@ -1,58 +1,79 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var express = require('express');
 var router = express.Router();
 var multiparty = require('multiparty');
 var fs = require("fs");
 var path = require('path');
+var jwt = require('jsonwebtoken');
 router.post('/upload', function (req, res) {
-    //console.log(path.resolve(__dirname,'get-upload'));
-    var form = new multiparty.Form({ uploadDir: path.resolve(__dirname, '../public/upload/') });
-    var result;
-    form.parse(req, function (err, fields, files) {
-        if (err) {
-            console.log(err);
-        }
-        if(fields){
-            console.log(fields);
-        }
-        //分两种情况，一: 单个提交方式二：formData提交即全部提交
-        if (files.file) {
-            console.log(files.file);
-            // try {
-            //     fs.renameSync(files.file[0].path,path.resolve(__dirname,'../public/upload/'+targetFile+'/'+files.file[0].originalFilename));
-            //     result={result:true,message:'上传成功'};
-            //     console.log('单个提交上传成功');
-            //     res.send(result);
-            //     res.end();
-            // }
-            // catch (err){
-            //     result={result:false,message:err};
-            //     console.log(err);
-            //     res.send(result);
-                result={response:'上传成功啦！',error:false,message:null};
-                res.send(result);
-                res.end();
-            // }
-        }
-        else {
-            // console.log('all');
-            // try{
-            //     for(var key in files){
-            //         //console.log(files[key][0].path);
-            //         fs.renameSync(files[key][0].path,path.resolve(__dirname,'../public/upload/'+targetFile+'/'+files[key][0].originalFilename));
-            //     }
-            //     result={result:true,message:'上传成功'};
-            //     console.log('all提交上传成功');
-            //     res.send(result);
-            //     res.end();
-            // }
-            // catch (err){
-            //     result={result:false,message:'上传失败'};
-            //     console.log(err);
-            //     res.send(result);
-            //     res.end();
-            // }
-        }
-    });
-    res.type('json');
+    var typeCheck = req.is('multipart/form-data');
+    console.log(typeCheck);
+    if (typeCheck) {
+        var form = new multiparty.Form({ uploadDir: path.resolve(__dirname, '../public/upload/') });
+        var result;
+        form.parse(req, function (err, fields, files) {
+            if (err) {
+                console.log(err);
+            }
+            //分两种情况，一: 单个提交方式二：formData提交即全部提交
+            if (files) {
+                console.log('get file');
+                // try {
+                //     fs.renameSync(files.file[0].path,path.resolve(__dirname,'../public/upload/'+targetFile+'/'+files.file[0].originalFilename));
+                //     result={result:true,message:'上传成功'};
+                //     console.log('单个提交上传成功');
+                //     res.send(result);
+                //     res.end();
+                // }
+                // catch (err){
+                //     result={result:false,message:err};
+                //     console.log(err);
+                //     res.send(result);
+                //     res.end();
+                // }
+            }
+            else {
+                // console.log('all');
+                // try{
+                //     for(var key in files){
+                //         //console.log(files[key][0].path);
+                //         fs.renameSync(files[key][0].path,path.resolve(__dirname,'../public/upload/'+targetFile+'/'+files[key][0].originalFilename));
+                //     }
+                //     result={result:true,message:'上传成功'};
+                //     console.log('all提交上传成功');
+                //     res.send(result);
+                //     res.end();
+                // }
+                // catch (err){
+                //     result={result:false,message:'上传失败'};
+                //     console.log(err);
+                //     res.send(result);
+                //     res.end();
+                // }
+            }
+        });
+    }
+    else {
+        res.status(404).send({ res: '类型错误!' });
+    }
+    res.end();
 });
+router.post('/login', function (req, res) {
+    var older_token = jwt.sign({ foo: 'bar', iat: Math.floor(Date.now() / 1000) - 30 }, 'heiheihei');
+    var r = { success: true, message: null, result: older_token };
+    console.log(req.body);
+    console.log(req.query);
+    console.log(req.params);
+    var receiveToken = req.get('Authorization');
+    console.log(receiveToken);
+    res.send(r);
+    res.end();
+});
+var ResponseModel = (function () {
+    function ResponseModel() {
+    }
+    return ResponseModel;
+}());
+exports.ResponseModel = ResponseModel;
 module.exports = router;
--- routes/server.ts
+++ routes/server.ts
@@ -3,52 +3,74 @@ var router = express.Router();
 var multiparty = require('multiparty');
 var fs = require("fs");
 var path=require('path');
+var jwt = require('jsonwebtoken');
 router.post('/upload', function(req, res) {
-    //console.log(path.resolve(__dirname,'get-upload'));
-    var form = new multiparty.Form({uploadDir:path.resolve(__dirname,'../public/upload/')});
-    var result;
-    form.parse(req, function(err, fields, files) {
-        if(err){
-            console.log(err);
-        }
-        //分两种情况，一: 单个提交方式二：formData提交即全部提交
-        if(files.file){
-            console.log('get file');
-            // try {
-            //     fs.renameSync(files.file[0].path,path.resolve(__dirname,'../public/upload/'+targetFile+'/'+files.file[0].originalFilename));
-            //     result={result:true,message:'上传成功'};
-            //     console.log('单个提交上传成功');
-            //     res.send(result);
-            //     res.end();
-            // }
-            // catch (err){
-            //     result={result:false,message:err};
-            //     console.log(err);
-            //     res.send(result);
-            //     res.end();
-            // }
-        }
-        else{
-            // console.log('all');
-            // try{
-            //     for(var key in files){
-            //         //console.log(files[key][0].path);
-            //         fs.renameSync(files[key][0].path,path.resolve(__dirname,'../public/upload/'+targetFile+'/'+files[key][0].originalFilename));
-            //     }
-            //     result={result:true,message:'上传成功'};
-            //     console.log('all提交上传成功');
-            //     res.send(result);
-            //     res.end();
-            // }
-            // catch (err){
-            //     result={result:false,message:'上传失败'};
-            //     console.log(err);
-            //     res.send(result);
-            //     res.end();
-            // }
-        }
-    });
-    res.type('json');
-    res.end('get');
+   let typeCheck=req.is('multipart/form-data');
+    console.log(typeCheck);
+    if(typeCheck){
+        var form = new multiparty.Form({uploadDir:path.resolve(__dirname,'../public/upload/')});
+        var result;
+        form.parse(req, function(err, fields, files) {
+            if(err){
+                console.log(err);
+            }
+            //分两种情况，一: 单个提交方式二：formData提交即全部提交
+            if(files){
+                console.log('get file');
+                // try {
+                //     fs.renameSync(files.file[0].path,path.resolve(__dirname,'../public/upload/'+targetFile+'/'+files.file[0].originalFilename));
+                //     result={result:true,message:'上传成功'};
+                //     console.log('单个提交上传成功');
+                //     res.send(result);
+                //     res.end();
+                // }
+                // catch (err){
+                //     result={result:false,message:err};
+                //     console.log(err);
+                //     res.send(result);
+                //     res.end();
+                // }
+            }
+            else{
+                // console.log('all');
+                // try{
+                //     for(var key in files){
+                //         //console.log(files[key][0].path);
+                //         fs.renameSync(files[key][0].path,path.resolve(__dirname,'../public/upload/'+targetFile+'/'+files[key][0].originalFilename));
+                //     }
+                //     result={result:true,message:'上传成功'};
+                //     console.log('all提交上传成功');
+                //     res.send(result);
+                //     res.end();
+                // }
+                // catch (err){
+                //     result={result:false,message:'上传失败'};
+                //     console.log(err);
+                //     res.send(result);
+                //     res.end();
+                // }
+            }
+        });
+    }
+    else{
+        res.status(404).send({res:'类型错误!'});
+    }
+    res.end();
 });
+router.post('/login',function (req,res) {
+    var older_token = jwt.sign({ foo: 'bar', iat: Math.floor(Date.now() / 1000) - 30 }, 'heiheihei');
+    let r:ResponseModel={success:true,message:null,result:older_token};
+    console.log(req.body);
+    console.log(req.query);
+    console.log(req.params);
+    let receiveToken=req.get('Authorization');
+    console.log(receiveToken);
+    res.send(r);
+    res.end();
+});
+export class ResponseModel{
+    success:boolean;
+    message:string;
+    result:any;
+}
 module.exports = router;
