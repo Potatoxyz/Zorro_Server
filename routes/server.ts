48f90061d584fa66687dd48780ad06d68311834f
PotatoGG <512006634@qq.com>
2018-02-28 18:36:30 +0800

    guard

--- routes/server.ts
+++ routes/server.ts
@@ -7,65 +7,51 @@ var jwt = require('jsonwebtoken');
 router.post('/upload', function(req, res) {
    let typeCheck=req.is('multipart/form-data');
     console.log(typeCheck);
+    let receiveToken=req.get('Authorization');
     if(typeCheck){
-        var form = new multiparty.Form({uploadDir:path.resolve(__dirname,'../public/upload/')});
-        var result;
-        form.parse(req, function(err, fields, files) {
-            if(err){
-                console.log(err);
-            }
-            //分两种情况，一: 单个提交方式二：formData提交即全部提交
-            if(files){
-                console.log('get file');
-                // try {
-                //     fs.renameSync(files.file[0].path,path.resolve(__dirname,'../public/upload/'+targetFile+'/'+files.file[0].originalFilename));
-                //     result={result:true,message:'上传成功'};
-                //     console.log('单个提交上传成功');
-                //     res.send(result);
-                //     res.end();
-                // }
-                // catch (err){
-                //     result={result:false,message:err};
-                //     console.log(err);
-                //     res.send(result);
-                //     res.end();
-                // }
-            }
-            else{
-                // console.log('all');
-                // try{
-                //     for(var key in files){
-                //         //console.log(files[key][0].path);
-                //         fs.renameSync(files[key][0].path,path.resolve(__dirname,'../public/upload/'+targetFile+'/'+files[key][0].originalFilename));
-                //     }
-                //     result={result:true,message:'上传成功'};
-                //     console.log('all提交上传成功');
-                //     res.send(result);
-                //     res.end();
-                // }
-                // catch (err){
-                //     result={result:false,message:'上传失败'};
-                //     console.log(err);
-                //     res.send(result);
-                //     res.end();
-                // }
-            }
-        });
+        if(receiveToken){
+            jwt.verify(receiveToken, 'heiheihei', function(err, decoded) {
+                console.log(decoded);
+                if(err){
+                    res.status(407).send({res:'授权错误!'});
+                    res.end();
+                }
+                else{
+                    var form = new multiparty.Form({uploadDir:path.resolve(__dirname,'../public/upload/')});
+                    form.parse(req, function(err, fields, files) {
+                        if(files){
+                            console.log('get file');
+                            res.end();
+                        }
+                    })
+                }
+            });
+        }
+        else {
+            res.status(403).send({res:'未授权，请先登录!'});
+        }
     }
     else{
-        res.status(404).send({res:'类型错误!'});
+        res.status(400).send({res:'类型错误!'});
     }
     res.end();
 });
 router.post('/login',function (req,res) {
-    var older_token = jwt.sign({ foo: 'bar', iat: Math.floor(Date.now() / 1000) - 30 }, 'heiheihei');
-    let r:ResponseModel={success:true,message:null,result:older_token};
-    console.log(req.body);
-    console.log(req.query);
-    console.log(req.params);
-    let receiveToken=req.get('Authorization');
-    console.log(receiveToken);
-    res.send(r);
+    let userName=req.body.userName;
+    let password=req.body.password;
+    if(userName=='10086'&&password=='123123'){
+        let older_token = jwt.sign({ userName: userName, iat: Math.floor(Date.now() / 1000) - 30 }, 'heiheihei');
+        let r:ResponseModel={success:true,message:null,result:older_token};
+        console.log(req.body);
+        console.log(req.query);
+        console.log(req.params);
+        let receiveToken=req.get('Authorization');
+        console.log(receiveToken);
+        res.send(r);
+    }
+    else{
+        let r:ResponseModel={success:false,message:'用户名或密码错误！',result:null};
+    }
     res.end();
 });
 export class ResponseModel{
